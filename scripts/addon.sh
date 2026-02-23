c7c5c254967b6e7db635f171b45877c28a7c6fce
Anson Qian <anson627@gmail.com>
2026-02-23 16:07:04 -0500

    add addon

---
+++ scripts/addon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source scripts/variables.sh
+
+cilium install --version 1.19.1 --set azure.resourceGroup="${RESOURCE_GROUP}"
+
+kubectl label nodes -l agentpool=${USER_POOL_NAME} nvidia.com/gpu.present=true --overwrite
+
+helm upgrade --install dra-driver nvidia/nvidia-dra-driver-gpu \
+    --version=25.12.0 \
+    --create-namespace \
+    --namespace nvidia \
+    -f configs/dra-driver-values.yaml \
+    --wait
+
+helm repo add kuberay https://ray-project.github.io/kuberay-helm/
+helm repo update
+helm install kuberay-operator kuberay/kuberay-operator \
+    --version 1.5.1 \
+    --create-namespace \
+    --namespace ray \
+    --wait
