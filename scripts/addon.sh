ab25018680133618dc02841e1ea0a8cdc2ed3ab3
Anson Qian <anson627@gmail.com>
2026-02-24 12:18:55 -0500

    add device plugin

--- scripts/addon.sh
+++ scripts/addon.sh
@@ -8,6 +8,13 @@ cilium install --version 1.19.1 --set azure.resourceGroup="${RESOURCE_GROUP}"
 
 kubectl label nodes -l agentpool=${USER_POOL_NAME} nvidia.com/gpu.present=true --overwrite
 
+helm upgrade --install nvdp nvdp/nvidia-device-plugin \
+    --version=0.18.2 \
+    --create-namespace \
+    --namespace nvidia \
+    -f configs/device-plugin-values.yaml \
+    --wait
+
 helm upgrade --install dra-driver nvidia/nvidia-dra-driver-gpu \
     --version=25.12.0 \
     --create-namespace \
